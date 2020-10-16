b70739410f2aa242ba61e510633d3c191854191a
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-16 01:03:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  array.map { |element| element*element }  # Use an Enumerable to square every element in the passed in array
+  array.map { |e|e*e }  # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
 end
 
